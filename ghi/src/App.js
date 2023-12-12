ec5a8424d9164145aaac0083a6180c675e4772f7
John An <junghooonan@gmail.com>
2023-12-11 20:28:31 -0800

    added Card Form

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -2,10 +2,12 @@ import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import BudgetForm from "./Budget/BudgetForm.js";
+import CardForm from "./Card/CardForm.js";
 
 
 const router = createBrowserRouter([
   { path: "budgets/new", Component: BudgetForm },
+  { path: "cards/new", Component: CardForm },
 ]);
 
 function App() {
