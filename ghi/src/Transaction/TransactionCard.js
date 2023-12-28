6daaa9ebf679a8dd1cbfebf456ceda8ec6650a7d
John An <junghooonan@gmail.com>
2023-12-28 14:11:05 -0800

    added Transactions component

--- ghi/src/Transaction/TransactionCard.js
+++ ghi/src/Transaction/TransactionCard.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 export const TransactionCard = (props) => {
     return (
         <div className="col">
--- ghi/src/Transaction/TransactionList.js
+++ ghi/src/Transaction/TransactionList.js
@@ -1,19 +1,38 @@
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { TransactionCard } from "./TransactionCard";
+import { Link } from "react-router-dom";
 
-export const TransactionList = () => {
-    const transactions = useSelector(state => state.transactions);
+const TransactionList = () => {
+    const [transactions, setTransactions] = useState([]);
+
+    const fetchData = async() => {
+        const response = await fetch(
+            `${process.env.REACT_APP_API_HOST}/transactions`
+        );
+        if (response.ok) {
+            const data = await response.json();
+            console.log('DATA: ', data);
+            setTransactions(data);
+        } else {
+            console.error(response);
+            return response.status(500).json({ error: "Transactions data fetch unsuccessful" });
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     return (
         <>
         <div className="px-4 py-5 mt-0 text-center bg-info">
-            <h1 className="display-5 fw-bold">Your Budgets</h1>
+            <h1 className="display-5 fw-bold"></h1>
             <div className="col-lg-6 mx-auto">
                 <p className="lead mb-4">
-                    The only resource you'll ever need to plan an run your in-person or
-                    virtual conference for thousands of attendees and presenters.
+                    The resource that you can count on to track your spending and monitor transaction details on your card.
                 </p>
                 <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                    <Link to="/budgets/create" className="btn btn-primary btn-lg px-4 gap-3">New Budget</Link>
+                    <Link to="/transactions/create" className="btn btn-primary btn-lg px-4 gap-3">New Transaction</Link>
                 </div>
             </div>
         </div>
@@ -30,3 +49,5 @@ export const TransactionList = () => {
         </>
     );
 };
+
+export default TransactionList;
